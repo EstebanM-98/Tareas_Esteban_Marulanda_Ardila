fd3186dd80dfdd8b478cff37187e1a1b84b1a936
EstebanM-98 <70489292+EstebanM-98@users.noreply.github.com>
2020-09-06 13:31:50 -0500

    Update test.py

--- Tarea1/test.py
+++ Tarea1/test.py
@@ -4,15 +4,15 @@ import funciones
 f=120
 if funciones.Factorial(5)==f:
   print('True')
-  else:
-    print('False')
+else:
+  print('False')
     
 #En la cálculadora, 20 combinado 15 es 15504
 b=15504
 if funciones.Binomial(20,15)==b:
   print('True')
-  else:
-    print('False')
+else:
+  print('False')
     
 #La función Pascal(n) se puede testear yendo al documento txt creado y verificando con los coeficientes de n=2 (muy conocido)
 funciones.Pascal(2)
