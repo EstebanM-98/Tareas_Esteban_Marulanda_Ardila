d1ecaea25cf51b0050d39c9c6585697c96edbe91
EstebanM-98 <70489292+EstebanM-98@users.noreply.github.com>
2020-11-28 23:12:37 -0500

    Create Proyecto_final.py

---
+++ Proyecto/Proyecto_final.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.linalg as scl
+from matplotlib import pyplot as plt
+from matplotlib.animation import FuncAnimation
+import Schrodinger
