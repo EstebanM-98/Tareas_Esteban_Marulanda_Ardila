3365a37da139ac14ae0bdefd3f8cfc9cd300ff0a
EstebanM-98 <70489292+EstebanM-98@users.noreply.github.com>
2020-11-29 22:21:54 -0500

    Update Proyecto_final.py

--- Proyecto/Proyecto_final.py
+++ Proyecto/Proyecto_final.py
@@ -1,6 +1,1 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import scipy.linalg as scl
-from matplotlib import pyplot as plt
-from matplotlib.animation import FuncAnimation
-import Schrodinger
+
